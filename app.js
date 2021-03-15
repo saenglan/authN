00d9424e2890b55967f704776294ba54bcf90bbb
Sam England <77686863+sam-eng11@users.noreply.github.com>
2021-03-15 09:42:14 -0400

    Hash Passwords

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-//Level 2:
-const encrypt = require('mongoose-encryption');
+// //Level 2:
+// const encrypt = require('mongoose-encryption');
+
+//Level 3:
+const md5 = require("md5");
 
 const app = express();
 
@@ -19,19 +22,23 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 //Level 2:
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
+//Level 2:
 //MOVE secret to .env
 //const secret = "Thisisourlittlesecret.";
 //Add before you develop the model
 //Encrypt only selected fields
 //Connects to the .env file
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+//userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 // //Level 1:
 // const userSchema = {
@@ -41,42 +48,46 @@ userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["pass
 
 const User = new mongoose.model("User", userSchema);
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
   res.render("home");
 });
 
-app.get("/login", function(req, res){
+app.get("/login", function(req, res) {
   res.render("login");
 });
 
-app.get("/register", function(req, res){
+app.get("/register", function(req, res) {
   res.render("register");
 });
 
-app.post("/register", function(req, res){
+app.post("/register", function(req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    //password: req.body.password
+    //Level 3: turn password into Hash
+    password: md5(req.body.password)
   });
-//Level 2: Mongoose encrypts on Save
-  newUser.save(function(err){
+  //Level 2: Mongoose encrypts on Save
+  newUser.save(function(err) {
     if (err) {
       console.log(err);
-    } else{
+    } else {
       res.render("secrets");
     }
   });
 });
 
-app.post("/login", function(req, res){
+app.post("/login", function(req, res) {
   const username = req.body.username;
   const password = req.body.password;
-//Level 2: Decrypts on find/findOne
-  User.findOne({email: username}, function(err, foundUser){
+  //Level 2: Decrypts on find/findOne
+  User.findOne({
+    email: username
+  }, function(err, foundUser) {
     if (err) {
       console.log(err);
     } else {
-      if(foundUser){
+      if (foundUser) {
         if (foundUser.password === password) {
           res.render("secrets");
         }
